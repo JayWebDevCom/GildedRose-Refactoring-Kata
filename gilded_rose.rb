fcdc37a933f45a9399d86e5cacbc6adafc36d62a
JayWebDevCom <jaiye.s@gmail.com>
2017-06-17 16:42:03 +0100

    extract into class specific methods of general class

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -5,6 +5,62 @@ class GildedRose
   end
 
   def sulfuras_update item
+    @item = General.new(item.sell_in, item.quality)
+    @item.sulfuras_update item
+  end
+
+  def backstage_update item
+    @item = General.new(item.sell_in, item.quality)
+    @item.backstage_update item
+  end
+
+  def aged_brie_update item
+    @item = General.new(item.sell_in, item.quality)
+    @item.aged_brie_update item
+  end
+
+  def general_update item
+    @item = General.new(item.sell_in, item.quality)
+    @item.general_update item
+  end
+
+  def update_quality()
+    @items.each do |item|
+    case item.name
+    when 'Sulfuras, Hand of Ragnaros'
+      sulfuras_update item
+    when 'Backstage passes to a TAFKAL80ETC concert'
+      backstage_update item
+    when 'Aged Brie'
+      aged_brie_update item
+    else
+      general_update item
+    end
+    end
+  end
+
+end
+
+class General
+  attr_accessor :sell_in, :quality
+
+  def initialize(sell_in, quality)
+    @sell_in = sell_in
+    @quality = quality
+  end
+
+  def general_update item
+    item.sell_in -= 1
+    return if item.quality == 0
+    item.quality -= 1
+    item.quality -= 1 if item.sell_in < 0 && item.quality > 0
+  end
+
+  def aged_brie_update item
+    item.sell_in -= 1
+    return if item.quality == 50
+    item.quality += 1
+    item.quality += 1 if item.sell_in < 0
   end
 
   def backstage_update item
@@ -16,39 +72,11 @@ class GildedRose
     item.quality += 1 if item.sell_in < 5
   end
 
-  def update_aged_brie item
-    item.sell_in -= 1
-    return if item.quality == 50
-    item.quality += 1
-    item.quality += 1 if item.sell_in < 0
-  end
-
-  def unspecified_update item
-    item.sell_in -= 1
-    return if item.quality == 0
-    item.quality -= 1
-    item.quality -= 1 if item.sell_in < 0 && item.quality > 0
-  end
-
-  def update_quality()
-
-    @items.each do |item|
-
-    case item.name
-    when 'Sulfuras, Hand of Ragnaros'
-      sulfuras_update item
-    when 'Backstage passes to a TAFKAL80ETC concert'
-      backstage_update item
-    when 'Aged Brie'
-      update_aged_brie item
-    else
-      unspecified_update item
-    end
-
-    end
+  def sulfuras_update item
   end
 
 end
+
 
 class Item
   attr_accessor :name, :sell_in, :quality
--- spec/unspecified_item_spec.rb
+++ spec/unspecified_item_spec.rb
@@ -3,19 +3,19 @@ require './gilded_rose'
 describe "Unspecified Item" do
   context 'to_s()' do
     it 'returns string output' do
-      item = Item.new 'Unspecified item', 12, 12
-      expect(item.to_s).to eq 'Unspecified item, 12, 12'
+      item = Item.new 'General item', 12, 12
+      expect(item.to_s).to eq 'General item, 12, 12'
     end
   end
 end
 
-describe 'Product #update_quality' do  
+describe 'Product #update_quality' do
 
   describe "Unspecified Item" do
 
     context 'sell-in is 20, quality is 20' do
       it "quality decreases by 1, sell_in decreases by 1" do
-        items = [Item.new("Unspecified item", 20, 20)]
+        items = [Item.new("General item", 20, 20)]
         GildedRose.new(items).update_quality()
         expect(items[0].quality).to eq 19
         expect(items[0].sell_in).to eq 19
@@ -24,7 +24,7 @@ describe 'Product #update_quality' do
 
     context 'sell-in is 1, quality is 1' do
       it "quality decreases by 1, sell_in decreases by 1" do
-        items = [Item.new("Unspecified item", 1, 1)]
+        items = [Item.new("General item", 1, 1)]
         GildedRose.new(items).update_quality()
         expect(items[0].quality).to eq 0
         expect(items[0].sell_in).to eq 0
@@ -33,7 +33,7 @@ describe 'Product #update_quality' do
 
     context 'sell-in is 0, quality is 0' do
       it "quality never negative, sell_in decreases by 1" do
-        items = [Item.new("Unspecified item", 0, 0)]
+        items = [Item.new("General item", 0, 0)]
         GildedRose.new(items).update_quality()
         expect(items[0].quality).to eq 0
         expect(items[0].sell_in).to eq -1
@@ -42,7 +42,7 @@ describe 'Product #update_quality' do
 
     context 'sell-in is -1, quality is 0' do
       it "quality never negative, sell_in decreases by 1" do
-        items = [Item.new("Unspecified item", -1, 0)]
+        items = [Item.new("General item", -1, 0)]
         GildedRose.new(items).update_quality()
         expect(items[0].sell_in).to eq -2
         expect(items[0].quality).to eq 0
@@ -52,7 +52,7 @@ describe 'Product #update_quality' do
 
     context 'sell-in is -1, quality is 1' do
       it "quality never negative, sell_in decreases by 1" do
-        items = [Item.new("Unspecified item", -1, 1)]
+        items = [Item.new("General item", -1, 1)]
         GildedRose.new(items).update_quality()
         expect(items[0].sell_in).to eq -2
         expect(items[0].quality).to eq 0
@@ -61,7 +61,7 @@ describe 'Product #update_quality' do
 
     context 'sell-in is 5, quality is 5' do
       it "sell_in decreases by 1, quality decreases by 1 if sell-in is positive" do
-        items = [Item.new("Unspecified item", 5, 5)]
+        items = [Item.new("General item", 5, 5)]
         GildedRose.new(items).update_quality()
         expect(items[0].sell_in).to eq 4
         expect(items[0].quality).to eq 4
@@ -70,7 +70,7 @@ describe 'Product #update_quality' do
 
     context 'sell-in is negative, quality is 3' do
       it "quality never negative, quality decreases by 2 sell_in decreases by 1" do
-        items = [Item.new("Unspecified item", -1, 3)]
+        items = [Item.new("General item", -1, 3)]
         GildedRose.new(items).update_quality()
         expect(items[0].sell_in).to eq -2
         expect(items[0].quality).to eq 1
