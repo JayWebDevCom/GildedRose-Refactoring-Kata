7a185c4ad594abb02bf3b2c5f87fec26121ccb34
JayWebDevCom <jaiye.s@gmail.com>
2017-06-18 00:14:01 +0100

    add conjured first red test

---
+++ spec/conjured_spec.rb
@@ -0,0 +1,23 @@
+require './gilded_rose'
+
+describe 'Conjured' do
+  context 'to_s()' do
+    it 'returns string output' do
+      item = Item.new 'Conjured', 12, 12
+      expect(item.to_s).to eq 'Conjured, 12, 12'
+    end
+  end
+end
+
+describe 'Aged Brie' do
+  describe 'Product #update_quality' do
+    context 'sell-in is 20, quality is 20' do
+      it 'increases quality by 1, sell_in decreases by 1' do
+        items = [Item.new('Conjured', 20, 20)]
+        GildedRose.new(items).update_quality
+        expect(items[0].sell_in).to eq 19
+        expect(items[0].quality).to eq 18
+      end
+    end
+  end
+end
