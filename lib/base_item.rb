e058e32b80fa6018afbbf874831b8f5ca9aae6a9
JayWebDevCom <jaiye.s@gmail.com>
2017-06-18 20:40:17 +0100

    improve commenting

--- lib/base_item.rb
+++ lib/base_item.rb
@@ -1,4 +1,4 @@
-# inheritance provision of update(item) method
+# inheritance provision of update(item) method and SELL_IN_CHANGE constant
 class BaseItem
   SELL_IN_CHANGE = 1
   def update(item); end
