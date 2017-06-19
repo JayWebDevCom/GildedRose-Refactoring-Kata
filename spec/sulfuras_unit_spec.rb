a818a6bfcc0998d2c4a26b0c38b525b22557e1da
JayWebDevCom <jaiye.s@gmail.com>
2017-06-19 20:40:57 +0100

    add sulfuras unit spec

---
+++ spec/sulfuras_unit_spec.rb
@@ -0,0 +1,24 @@
+require './gilded_rose'
+require './lib/base_item'
+
+describe 'Sulfuras, Hand of Ragnaros' do
+    context 'legacy item' do
+      it 'sell-in never changes, quality never changes' do
+        sulfuras = Item.new('Sulfuras, Hand of Ragnaros', 20, 20)
+        sulfuras_class = BaseItem.new
+        sulfuras_class.update sulfuras
+        expect(sulfuras.quality).to eq 20
+        expect(sulfuras.sell_in).to eq 20
+      end
+    end
+
+    context 'legacy item' do
+      it 'sell-in never changes, quality never negative' do
+        sulfuras = Item.new('Sulfuras, Hand of Ragnaros', 0, 0)
+        sulfuras_class = BaseItem.new
+        sulfuras_class.update sulfuras
+        expect(sulfuras.quality).to eq 0
+        expect(sulfuras.sell_in).to eq 0
+      end
+    end
+end
