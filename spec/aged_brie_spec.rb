87cab7bea13d87d6339b60ed2d77dcf475bfb007
JayWebDevCom <jaiye.s@gmail.com>
2017-06-24 21:39:38 +0100

    define and implement gilded_rose_update refactor method for Aged Brie Specs

--- spec/aged_brie_spec.rb
+++ spec/aged_brie_spec.rb
@@ -1,4 +1,5 @@
 require './gilded_rose'
+require 'spec_helper'
 
 describe 'Aged Brie' do
   context 'to_s()' do
@@ -13,55 +14,49 @@ describe 'Aged Brie' do
   describe 'GildedRose update()' do
     context 'sell-in is 20, quality is 20' do
       it 'increases quality by 1, sell_in decreases by 1' do
-        items = [Item.new('Aged Brie', 20, 20)]
-        GildedRose.new(items).update_quality
-        expect(items[0].sell_in).to eq 19
-        expect(items[0].quality).to eq 21
+        Aged_Brie = gilded_rose_update('Aged Brie', 20, 20)
+        expect(Aged_Brie[:sell_in]).to eq 19
+        expect(Aged_Brie[:quality]).to eq 21
       end
     end
 
     context 'sell_in is 20, quality is 50' do
       it 'quality does not change above 50, sell_in still decreases by 1' do
-        items = [Item.new('Aged Brie', 20, 50)]
-        GildedRose.new(items).update_quality
-        expect(items[0].sell_in).to eq 19
-        expect(items[0].quality).to eq 50
+        Aged_Brie = gilded_rose_update('Aged Brie', 20, 50)
+        expect(Aged_Brie[:sell_in]).to eq 19
+        expect(Aged_Brie[:quality]).to eq 50
       end
     end
 
     context 'sell-in is 3, quality is 3' do
       it 'quality increases by 1, sell-in decreases by 1' do
-        items = [Item.new('Aged Brie', 3, 3)]
-        GildedRose.new(items).update_quality
-        expect(items[0].sell_in).to eq 2
-        expect(items[0].quality).to eq 4
+        Aged_Brie = gilded_rose_update('Aged Brie', 3, 3)
+        expect(Aged_Brie[:sell_in]).to eq 2
+        expect(Aged_Brie[:quality]).to eq 4
       end
     end
 
     context 'sell-in is 1, quality is 1' do
       it 'quality increases by 1, sell-in decreases by 1' do
-        items = [Item.new('Aged Brie', 1, 1)]
-        GildedRose.new(items).update_quality
-        expect(items[0].sell_in).to eq 0
-        expect(items[0].quality).to eq 2
+        Aged_Brie = gilded_rose_update('Aged Brie', 1, 1)
+        expect(Aged_Brie[:sell_in]).to eq 0
+        expect(Aged_Brie[:quality]).to eq 2
       end
     end
 
     context 'sell-in is 0 or less, quality is 0' do
       it 'quality increases by 2, sell-in decreases by 1' do
-        items = [Item.new('Aged Brie', 0, 0)]
-        GildedRose.new(items).update_quality
-        expect(items[0].sell_in).to eq(-1)
-        expect(items[0].quality).to eq 2
+        Aged_Brie = gilded_rose_update('Aged Brie', 0, 0)
+        expect(Aged_Brie[:sell_in]).to eq(-1)
+        expect(Aged_Brie[:quality]).to eq 2
       end
     end
 
     context 'sell-in is 4, quality is 4' do
       it 'quality increases by 1, sell-in decreases by 1' do
-        items = [Item.new('Aged Brie', 4, 4)]
-        GildedRose.new(items).update_quality
-        expect(items[0].sell_in).to eq 3
-        expect(items[0].quality).to eq 5
+        Aged_Brie = gilded_rose_update('Aged Brie', 4, 4)
+        expect(Aged_Brie[:sell_in]).to eq 3
+        expect(Aged_Brie[:quality]).to eq 5
       end
     end
   end
